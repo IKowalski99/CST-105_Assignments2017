af5e6d21e322bbb3d6c57ce6322b5800ee9869c5
LAPTOP\Brian Cross <isaiahkow99@gmail.com>
2017-06-18 21:46:46 -0700

    First assignment

---
+++ Topic 1/AssignmentOne.java
@@ -0,0 +1,8 @@
+
+public class AssignmentOne {
+	public static void main(String[] args){
+		System.out.println("My name is Isaiah.");
+		System.out.println("My favorite color is black.");
+		System.out.println("If I won the lottery, I would invest.");
+	}
+}
---
+++ Topic 1/Introduction.java
@@ -0,0 +1,6 @@
+
+public class Introduction {
+	public static void main(String[] args) {
+
+	}
+}
