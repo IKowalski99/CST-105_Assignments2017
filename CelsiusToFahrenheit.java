36b36845ac728be464e0149d9677e5cec8f90589
IKowalski99 <isaiahkow99@gmail.com>
2017-06-25 15:08:52 -0700

    Topic 2

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,20 @@
+//Isaiah Kowalski, this work is mine
+public class CelsiusToFahrenheit {
+	public static void main(String [] args){
+	
+		//Conversion for Celsius to Fahrenheit
+		double c;
+		double f;
+		
+		//Enter value for c (Celsius)
+		c = 47;
+		
+		//Convert Celsius to Fahrenheit
+		f = c * 9 / 5 + 32;
+				
+		//Print results
+		System.out.println(c + " Celsius is equivalent to " + f + " Fahrenheit");
+		
+	}
+
+}
